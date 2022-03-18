72b41b752ddfdd61b4b5fa95f7ecc503ce5addf9
shiraz27 <hosniishiraz@gmail.com>
2022-03-18 11:04:33 +0100

    all files including node modules ready for hello world

---
+++ ios/Pods/Target Support Files/Pods-ReactNativeWebApp-ReactNativeWebAppTests/Pods-ReactNativeWebApp-ReactNativeWebAppTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ReactNativeWebApp_ReactNativeWebAppTests : NSObject
+@end
+@implementation PodsDummy_Pods_ReactNativeWebApp_ReactNativeWebAppTests
+@end
---
+++ src/index.js
@@ -0,0 +1,12 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import App from './App';
+import name from './app.json';
+
+AppRegistry.registerComponent(name, () => App);
+AppRegistry.runApplication(name, {
+  rootTag: document.getElementById('root'),
+});
